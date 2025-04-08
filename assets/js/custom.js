d8e7f58e4588eeb4b19f9cf2f44d730a2b5734d9
Esmond Agharovbokhan <133409203+Esmondsmith@users.noreply.github.com>
2025-04-08 15:47:43 +0100

    first on adhs 2025

---
+++ assets/js/custom.js
@@ -0,0 +1,154 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function () {
+
+        /*---------------------------------------------------
+            Main Slider
+        ----------------------------------------------------*/
+        $('.slider-carousel').owlCarousel({
+            loop: true,
+            navText: ['<i class="icofont icofont-simple-left"></i>', '<i class="icofont icofont-simple-right"></i>'],
+            nav: true,
+            autoplay: false,
+            autoplayTimeout: 5000,
+            animateOut: 'fadeOut',
+            animateIn: 'lightSpeedIn',
+            smartSpeed: 450,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            Screenshot Slider
+        ----------------------------------------------------*/
+        $('.screenshots-slider').owlCarousel({
+            loop: true,
+            navText: ['<i class="icofont icofont-simple-left"></i>', '<i class="icofont icofont-simple-right"></i>'],
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            margin: 10,
+            center: true,
+            responsive: {
+                0: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                991: {
+                    items: 3
+                },
+                1200: {
+                    items: 4
+                },
+                1920: {
+                    items: 4
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            About:- show more text
+        ----------------------------------------------------*/
+        $(".read-more-btn").click(function(){
+            $(".hidden-text").slideToggle();
+            const hiddenText = document.querySelector(".hidden-text");
+            if (hiddenText.classList.contains("show-text")) {
+                    hiddenText.classList.remove("show-text");
+                    this.innerHTML = 'Read More <i class="icofont icofont-double-right"></i>';
+                } else {
+                    hiddenText.classList.add("show-text");
+                    this.innerHTML = 'Read Less <i class="icofont icofont-double-left"></i>';
+                }
+        });
+
+        /*---------------------------------------------------
+            Testimonial Carousel
+        ----------------------------------------------------*/
+        $('.testimonial-carousel').owlCarousel({
+            loop: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                991: {
+                    items: 1
+                },
+                1200: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        });
+
+        /*---------------------------------------------------
+            Counter
+        ----------------------------------------------------*/
+        $('.counter-item span').counterUp({
+            delay: 10, // the delay time in ms
+            time: 1000 // the speed time in ms
+        });
+
+        /*---------------------------------------------------
+            Video PopUp
+        ----------------------------------------------------*/
+        $('.popup-video').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false
+        });
+
+        /*---------------------------------------------------
+            Smooth Scroll
+        ----------------------------------------------------*/
+        $('a[href*="#"]:not([href="#"]):not([data-toggle])').on('click', function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    }); 
+    /*---------------------------------------------------
+        Site Preloader
+    ----------------------------------------------------*/
+    $(window).on('load', function () {
+        $('.preloader').fadeOut(500);
+    });
+
+    new WOW().init();
+    //Dynamic year 
+    const currentYear = new Date().getFullYear();
+    document.getElementById('footerYear').textContent = currentYear;
+
+}(jQuery));
